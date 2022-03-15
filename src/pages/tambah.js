5fa5ff93e72f0a58aa86e73155009683844cc113
lazuardi100 <lazuardi.akhmad@gmail.com>
2022-03-15 10:10:57 +0700

    add user

--- src/pages/tambah.js
+++ src/pages/tambah.js
@@ -1,23 +1,68 @@
 import { useMutation, gql } from "@apollo/client";
+import { useNavigate } from "react-router-dom";
+
+const MUTATION = gql`
+  mutation AddCustomer($name: String!, $noHp: String!) {
+    addCustomer(input: { name: $name, noHp: $noHp }) {
+      namaCustomer
+    }
+  }
+`;
 
 function Tambah() {
-    const Daftarkan = () =>{
-        const nama = document.querySelector('#nama')
-        const hp = document.querySelector('#no_hp')
+  const [mutation, {data, loading, error}] = useMutation(MUTATION);
+  const navigate = useNavigate();
 
-        
-    }
-    return ( <div className="container mt-3">
+  if (loading) return 'Submitting...';
+  if (error) return `Submission error! ${error.message}`;
+
+  const Daftarkan = () => {
+    const nama = document.querySelector("#nama").value;
+    const hp = document.querySelector("#no_hp").value;
+
+    mutation({
+      variables: {
+        name: nama,
+        noHp: hp,
+      },
+    });
+
+    // console.log(data)
+    navigate("/customers");
+  };
+  return (
+    <div className="container mt-3">
+      <div class="mb-3">
+        <label for="" className="form-label">
+          Nama Calon Customer
+        </label>
+        <input
+          type="text"
+          class="form-control"
+          name="nama"
+          id="nama"
+          aria-describedby="helpId"
+          placeholder=""
+        />
         <div class="mb-3">
-          <label for="" className="form-label">Nama Calon Customer</label>
-          <input type="text" class="form-control" name="nama" id="nama" aria-describedby="helpId" placeholder=""/>
-          <div class="mb-3">
-            <label for="" className="form-label">Nomor HP</label>
-            <input type="text" class="form-control" name="no_hp" id="no_hp" aria-describedby="helpId" placeholder=""/>
-          </div>
+          <label for="" className="form-label">
+            Nomor HP
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            name="no_hp"
+            id="no_hp"
+            aria-describedby="helpId"
+            placeholder=""
+          />
         </div>
-        <button className="btn btn-primary">Simpan</button>
-    </div> );
+      </div>
+      <button onClick={Daftarkan} className="btn btn-primary">
+        Simpan
+      </button>
+    </div>
+  );
 }
 
 export default Tambah;
